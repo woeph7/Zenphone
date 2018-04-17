92ec2c2f4b7b290a047bdd4a0609d09872a34d9f
woeph7 <aeishin0212@gmail.com>
2018-04-17 10:43:26 +0200

    Finish static page animation

--- js/animate.js
+++ js/animate.js
@@ -4,8 +4,34 @@ var screenAnimateElements ={
     '.screen-1_heading',
     '.screen-1_phone',
     '.screen-1_shadow',
+  ],
+  '.screen-2' : [
+    '.screen-2_heading',
+    '.screen-2_subheading',
+    '.screen-2_phone',
+    '.screen-2_point_i_1',
+    '.screen-2_point_i_2',
+    '.screen-2_point_i_3',
+  ],
+  '.screen-3' :[
+    '.screen-3_heading',
+    '.screen-3_subheading',
+    '.screen-3_phone',
+    '.screen-3_features',
+  ],
+  '.screen-4' :[
+    '.screen-4_heading',
+    '.screen-4_subheading',
+    '.screen-4_type_item_i_1',
+    '.screen-4_type_item_i_2',
+    '.screen-4_type_item_i_3',
+    '.screen-4_type_item_i_4',
+  ],
+  '.screen-5' :[
+    '.screen-5_heading',
+    '.screen-5_subheading',
+    '.screen-5_bg',
   ]
-
 };
 
 function setScreenAnimate(screenCls) {
@@ -37,7 +63,7 @@ function setScreenAnimate(screenCls) {
       }
       isAnimateDone = true;
       return ;
-      } 
+      }
     //switch animateElements done -> init
     if(isAnimateDone === true){
       for(var i=0;i<animateElements.length;i++){
@@ -51,4 +77,7 @@ function setScreenAnimate(screenCls) {
   }
 }
 
-setScreenAnimate('.screen-1');
+
+for(k in screenAnimateElements){
+  setScreenAnimate(k);
+}
